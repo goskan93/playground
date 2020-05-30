37ddf2697f21ea19d91880c4038b6b6c974b2a7f
goskan93 <natalia.goska@gmail.com>
2020-05-30 15:46:59 -0300

    add cube

---
+++ src/components/Cube/Cube.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cube.css";
+
+const Cube = () => (
+  <div class="cubespinner">
+    <div class="face1">1</div>
+    <div class="face2">2</div>
+    <div class="face3">3</div>
+    <div class="face4">4</div>
+    <div class="face5">5</div>
+    <div class="face6">6</div>
+  </div>
+);
+
+export default Cube;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Home.css";
+import Cube from "../../components/Cube/Cube";
+
 const Home = () => (
   <div className="Home">
     <h1>Play online game</h1>
     <h3>Have some fun with our collection of fun, online games!</h3>
+
+    <div style={{ width: "120px", height: "120px", position: "relative", margin: "6rem auto" }}>
+      <Cube />
+    </div>
   </div>
 );
 
