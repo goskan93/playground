54b188e5d189c7667b5ff42eb37c9f52df997809
goskan93 <natalia.goska@gmail.com>
2020-06-07 11:39:51 -0300

    Updates

---
+++ precache-manifest.5190889c354d5d70c6c3047a81d7e754.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089f65e840ea43971e453768c1fa60aa",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "256e9dbe19a226120722",
+    "url": "/playground/static/css/3.17692c7b.chunk.css"
+  },
+  {
+    "revision": "203fc5ad53cc86fedd83",
+    "url": "/playground/static/css/4.cd73e28c.chunk.css"
+  },
+  {
+    "revision": "23a7ff90cbc3e42974ad",
+    "url": "/playground/static/css/5.f9e58f2c.chunk.css"
+  },
+  {
+    "revision": "622f79297f4215be7ba5",
+    "url": "/playground/static/css/main.d4100733.chunk.css"
+  },
+  {
+    "revision": "7652def1b8bf8028ea6c",
+    "url": "/playground/static/js/2.d5518f36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/playground/static/js/2.d5518f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256e9dbe19a226120722",
+    "url": "/playground/static/js/3.12a45911.chunk.js"
+  },
+  {
+    "revision": "203fc5ad53cc86fedd83",
+    "url": "/playground/static/js/4.969fb068.chunk.js"
+  },
+  {
+    "revision": "23a7ff90cbc3e42974ad",
+    "url": "/playground/static/js/5.67937d59.chunk.js"
+  },
+  {
+    "revision": "622f79297f4215be7ba5",
+    "url": "/playground/static/js/main.f5420d1d.chunk.js"
+  },
+  {
+    "revision": "30d9d2f4259e43708362",
+    "url": "/playground/static/js/runtime-main.b06dd00e.js"
+  }
+]);
