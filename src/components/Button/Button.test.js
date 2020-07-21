f499b93c272b18f3dbcdd5d46aaedf1708160230
goskan93 <natalia.goska@gmail.com>
2020-07-21 12:29:33 -0300

    fierst test btn

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -3,9 +3,24 @@ import { shallow } from "enzyme";
 import Button from "./Button";
 
 describe("<Button>", () => {
+  let wrapper = null;
+  beforeEach(() => {
+    wrapper = shallow(<Button />);
+  });
+  afterEach(() => {
+    wrapper = null;
+  });
   it("renders without error", () => {
-    const wrapper = shallow(<Button />);
-
-    console.log(wrapper.debug());
+    expect(wrapper).toHaveLength(1);
+  });
+  it("has default props", () => {
+    expect(wrapper.prop("disabled")).toBe(false);
+  });
+  it("correctly change props", () => {
+    wrapper.setProps({ disabled: true, label: "TestBtn", click: jest.fn(), btnClass: "Test" });
+    expect(wrapper.prop("disabled")).toBe(true);
+    expect(wrapper.prop("onClick")).toBeDefined();
+    expect(wrapper.text()).toBeDefined();
+    expect(wrapper.prop("className").split(" ")[1]).not.toBe("undefined");
   });
 });
