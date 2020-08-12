348590a6ca659823a1b84290d6a686bf4182e0f9
goskan93 <natalia.goska@gmail.com>
2020-08-12 15:26:08 +0200

    Navigation test

--- src/containers/Layout/Header/Header.test.js
+++ src/containers/Layout/Header/Header.test.js
@@ -3,18 +3,27 @@ import { shallow } from "enzyme";
 import Header, { NavigationMobile, NavigationDesktop, Navigation } from "./Header";
 
 describe("<Navigation/>", () => {
-  it("renders without error", () => {});
+  it.only("renders without error", () => {
+    const wrapper = shallow(<Navigation />);
+    expect(wrapper.find("Fragment")).toHaveLength(1);
+    wrapper.find("NavLink").forEach(node => expect(node.hasClass("Nav-Link")).toBeTruthy());
+    wrapper.find("NavLink").forEach(node => expect(node.prop("to")).toBeDefined());
+    wrapper.find("NavLink").forEach(node => expect(node.text()).toBeDefined());
+    console.log(wrapper.debug());
+  });
 });
-describe("<NavigationDesktop/>", () => {
-  it("renders without error", () => {});
-});
-describe("<NavigationMNavigationMobileobile/>", () => {
-  it("renders without error", () => {});
-  it("renders properly when drawer is opened", () => {});
-  it("renders properly when drawer is closed", () => {});
-  it("changes prop openDrawer when click", () => {});
-});
-describe("<Header/>", () => {
-  it("renders without error", () => {});
-  s;
-});
+// describe("<NavigationDesktop/>", () => {
+//   it("renders without error", () => {
+//     const wrapper = shallow(<NavigationDesktop />);
+//     // console.log(wrapper.debug());
+//   });
+// });
+// describe("<NavigationMNavigationMobileobile/>", () => {
+//   it("renders without error", () => {});
+//   it("renders properly when drawer is opened", () => {});
+//   it("renders properly when drawer is closed", () => {});
+//   it("changes prop openDrawer when click", () => {});
+// });
+// describe("<Header/>", () => {
+//   it("renders without error", () => {});
+// });
